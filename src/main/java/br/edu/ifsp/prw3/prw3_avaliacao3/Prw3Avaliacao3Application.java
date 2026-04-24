d58adbe2db66332a430c81eed0940917da58959e
FLCL84L <filipe.etecera@gmail.com>
2026-04-24 06:23:12 -0300

    EnableSpringDataWebSupport

--- src/main/java/br/edu/ifsp/prw3/prw3_avaliacao3/Prw3Avaliacao3Application.java
+++ src/main/java/br/edu/ifsp/prw3/prw3_avaliacao3/Prw3Avaliacao3Application.java
@@ -2,8 +2,11 @@ package br.edu.ifsp.prw3.prw3_avaliacao3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+// Filipe Bueno Balsani
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Prw3Avaliacao3Application {
 
 	public static void main(String[] args) {
