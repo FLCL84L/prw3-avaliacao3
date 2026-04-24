3cdb46a47b63e39f8c2dd33be64883815e7d0481
FLCL84L <filipe.etecera@gmail.com>
2026-04-24 05:28:17 -0300

    criacao de classes veiculo e mecanico e entidade conserto

---
+++ src/main/java/br/edu/ifsp/prw3/prw3_api_slides/mecanico/Mecanico.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.prw3.prw3_api_slides.mecanico;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Mecanico {
+    private String nome;
+    private Integer anosExperiencia;
+}
---
+++ src/main/java/br/edu/ifsp/prw3/prw3_api_slides/veiculo/Veiculo.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.prw3.prw3_api_slides.veiculo;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Veiculo {
+    private String marca;
+    private String modelo;
+    private String ano;
+}
